66df42cc221a6465ef429398feb5a16e92343b27
Tanishavats19 <63641969+Tanishavats19@users.noreply.github.com>
2023-09-29 00:31:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ToDoList from './components/toDoList';
 import Login from './authentication/Login';
 import { useDispatch, useSelector } from 'react-redux';
